cb4bce57da18c0d3e0965bb2632892f1cc3342ce
Renée Köcher <heals@codes.heals-draws.art>
2024-11-03 13:52:33 +0100

    apple/swift-syntax => swiftlang/swift-syntax

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", "509.0.0"..<"600.0.1")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"600.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
